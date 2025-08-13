d77c08620f0cf6105f4ca043b8e93795bc5cdf54
ben <ben@copia.io>
2025-08-13 16:59:01 -0400

    ensures header row when no titles exist

--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -1,8 +1,71 @@
 import * as React from "react";
 import { ReactDiffViewerRenderProps } from "../getLinesToRender";
+import { ReactDiffViewerStyles } from "./styles";
 
 interface TitleProps extends ReactDiffViewerRenderProps {
   largestPossibleLineNumber: number;
+}
+
+function AlignmentRow({
+  hideLineNumbers,
+  largestPossibleLineNumber,
+  splitView,
+}: {
+  hideLineNumbers: boolean;
+  largestPossibleLineNumber: number;
+  splitView: boolean;
+}) {
+  const largestLineNumberCharacters = String(largestPossibleLineNumber).length;
+  const gutterWidth = largestLineNumberCharacters * 10 + 20;
+  return (
+    <tr style={{ height: 0 }}>
+      {!hideLineNumbers && <td style={{ width: gutterWidth }} />}
+      <td style={{ width: 29 }} />
+      <td />
+      {splitView && (
+        <>
+          {!hideLineNumbers && <td style={{ width: gutterWidth }} />}
+          <td style={{ width: 29 }} />
+          <td />
+        </>
+      )}
+    </tr>
+  );
+}
+
+function TitleText({
+  hideLineNumbers,
+  leftTitle,
+  rightTitle,
+  splitView,
+  styles,
+}: {
+  hideLineNumbers: boolean;
+  leftTitle?: string | React.ReactElement;
+  rightTitle?: string | React.ReactElement;
+  splitView: boolean;
+  styles: ReactDiffViewerStyles;
+}) {
+  if (!leftTitle && !rightTitle) return <></>;
+
+  const colSpanOnSplitView = hideLineNumbers ? 2 : 3;
+  const colSpanOnInlineView = hideLineNumbers ? 2 : 4;
+
+  return (
+    <tr>
+      <td
+        colSpan={splitView ? colSpanOnSplitView : colSpanOnInlineView}
+        className={styles.titleBlock}
+      >
+        <pre className={styles.contentText}>{leftTitle}</pre>
+      </td>
+      {splitView && (
+        <td colSpan={colSpanOnSplitView} className={styles.titleBlock}>
+          <pre className={styles.contentText}>{rightTitle}</pre>
+        </td>
+      )}
+    </tr>
+  );
 }
 
 export function Title({
@@ -13,41 +76,20 @@ export function Title({
   styles,
   largestPossibleLineNumber,
 }: TitleProps) {
-  if (!leftTitle && !rightTitle) return <></>;
-
-  const colSpanOnSplitView = hideLineNumbers ? 2 : 3;
-  const colSpanOnInlineView = hideLineNumbers ? 2 : 4;
-
-  const largestLineNumberCharacters = String(largestPossibleLineNumber).length;
-  const gutterWidth = largestLineNumberCharacters * 10 + 20;
-
   return (
     <>
-      <tr style={{ height: 0 }}>
-        {!hideLineNumbers && <td style={{ width: gutterWidth }} />}
-        <td style={{ width: 29 }} />
-        <td />
-        {splitView && (
-          <>
-            {!hideLineNumbers && <td style={{ width: gutterWidth }} />}
-            <td style={{ width: 29 }} />
-            <td />
-          </>
-        )}
-      </tr>
-      <tr>
-        <td
-          colSpan={splitView ? colSpanOnSplitView : colSpanOnInlineView}
-          className={styles.titleBlock}
-        >
-          <pre className={styles.contentText}>{leftTitle}</pre>
-        </td>
-        {splitView && (
-          <td colSpan={colSpanOnSplitView} className={styles.titleBlock}>
-            <pre className={styles.contentText}>{rightTitle}</pre>
-          </td>
-        )}
-      </tr>
+      <AlignmentRow
+        hideLineNumbers={hideLineNumbers}
+        largestPossibleLineNumber={largestPossibleLineNumber}
+        splitView={splitView}
+      />
+      <TitleText
+        hideLineNumbers={hideLineNumbers}
+        leftTitle={leftTitle}
+        rightTitle={rightTitle}
+        splitView={splitView}
+        styles={styles}
+      />
     </>
   );
 }
